d592003db633f31d4f9455401b45a0818c925bea
Piyush Jain <87410986+Piyush5527@users.noreply.github.com>
2026-05-22 21:57:11 +0530

    INIT: adding server

---
+++ pc_health_server/src/main/java/com/example/pc_health_server/PcHealthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.pc_health_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PcHealthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PcHealthServerApplication.class, args);
+	}
+
+}
---
+++ pc_health_server/src/test/java/com/example/pc_health_server/PcHealthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pc_health_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcHealthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
